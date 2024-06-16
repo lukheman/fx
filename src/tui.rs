7a491f3e679015ace110c411ded2502a4277dd06
Akmal <akmalthemalong@gmail.com>
2024-06-16 17:27:40 +0800

    first commit

---
+++ src/tui.rs
@@ -0,0 +1,32 @@
+use std::io::{self, stdout, Stdout};
+
+
+use crossterm::{
+    execute, 
+    terminal::{
+        enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen
+    }
+};
+
+
+use ratatui::{
+    prelude::{Terminal, CrosstermBackend}
+};
+
+pub type Tui = Terminal<CrosstermBackend<Stdout>>;
+
+pub fn init() -> io::Result<Tui> {
+
+    execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+    Terminal::new(CrosstermBackend::new(stdout()))
+
+}
+
+pub fn restore() -> io::Result<()> {
+    execute!(stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+
+    Ok(())
+}
